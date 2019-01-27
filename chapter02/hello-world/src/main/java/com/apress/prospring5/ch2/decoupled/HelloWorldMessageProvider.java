67c47ae90032f9939e77f48b465b9a46a8795234
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-01-27 13:07:31 -0400

    Readme.txt and compilando y ejecutando solo el capitulo 2

---
+++ chapter02/hello-world/src/main/java/com/apress/prospring5/ch2/decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,18 @@
+package com.apress.prospring5.ch2.decoupled;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+    public HelloWorldMessageProvider(){
+        System.out.println(" --> HelloWorldMessageProvider: constructor caled");
+    }
+
+    @Override
+    public String getMessage() {
+        return "Hello World!";
+    }
+
+    /*public static void main(String... args) {
+        HelloWorldMessageProvider helloWorldMessageProvider = new HelloWorldMessageProvider();
+        System.out.println("xxxgetMessage() = " + helloWorldMessageProvider.getMessage());
+    }*/
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Gradle version 4.4.1
+java 1.8
+
+1-) gradle clean build
